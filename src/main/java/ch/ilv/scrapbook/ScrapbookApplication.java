f4a8f5fe85a00101701dfa93df46e0152eef5ce8
lauchbertia <sabrina.boccia@bbzbl-it.ch>
2023-04-21 10:55:42 +0200

    Init

---
+++ src/main/java/ch/ilv/scrapbook/ScrapbookApplication.java
@@ -0,0 +1,13 @@
+package ch.ilv.scrapbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrapbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/ilv/scrapbook/ScrapbookApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.ilv.scrapbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
